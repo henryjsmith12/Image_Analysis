3c5bd9766110d026c9942375d9640876887bd347
henryjsmith12 <henryjsmith12@gmail.com>
2021-05-19 20:46:39 -0500

    Added setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==============================================================================
+
+from setuptools import setup
+
+# ==============================================================================
+
+setup(
+    name='Image_Analysis',
+    version='0.0.0',
+    description='',
+    author='Henry Smith',
+    author_email='smithh@anl.gov',
+    url='https://github.com/henryjsmith12/Image_Analysis',
+    install_requires=['pyqtgraph',
+                      'matplotlib',
+                      'numpy',
+                      ],
+    license='See LICENSE File',
+    platforms='any',
+)
