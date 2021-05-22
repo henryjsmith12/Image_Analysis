6600a4204b045d3dceccab8a94766391f40de8c1
henryjsmith12 <henryjsmith12@gmail.com>
2021-05-22 15:02:10 -0500

    Profile plots now update when the image viewbox is changed

--- source/widgets.py
+++ source/widgets.py
@@ -40,6 +40,7 @@ class OptionsWidget(pg.LayoutWidget):
         self.options_gbox.setLayout(self.options_layout)
         self.files_gbox.setLayout(self.files_layout)
 
+    # --------------------------------------------------------------------------
 
     def setupComponents(self):
         # Create options widgets
@@ -58,8 +59,9 @@ class OptionsWidget(pg.LayoutWidget):
         # Link widgets to actions
         self.browse_btn.clicked.connect(self.openDirectory)
         self.clear_btn.clicked.connect(self.clearFileList)
-        self.file_list.itemDoubleClicked.connect(self.loadFile)
+        self.file_list.itemClicked.connect(self.loadFile)
 
+    # --------------------------------------------------------------------------
 
     def openDirectory(self):
         # Find directory with image files
@@ -72,10 +74,12 @@ class OptionsWidget(pg.LayoutWidget):
         self.file_list.clear()
         self.file_list.addItems(files)
 
+    # --------------------------------------------------------------------------
 
     def clearFileList(self):
         self.file_list.clear()
 
+    # --------------------------------------------------------------------------
 
     def loadFile(self, file):
         #Concatenate directory and file names
@@ -95,6 +99,7 @@ class AnalysisWidget(pg.LayoutWidget):
         super(AnalysisWidget, self).__init__(parent)
         self.main_window = parent
 
+    # --------------------------------------------------------------------------
 
     def setupComponents(self):
         ...
@@ -113,35 +118,69 @@ class ImageWidget(pg.ImageView):
         super(ImageWidget, self).__init__(parent)
         self.main_window = parent
 
+        # Delete unnecessary buttons
+        self.ui.roiBtn.hide()
+        self.ui.menuBtn.hide()
+
+    # --------------------------------------------------------------------------
 
     def displayImage(self, file_path):
-        # Read and set image
-        self.image = plt.imread(file_path, format="RGB")
+        # Read and set image file
+        self.image = plt.imread(file_path)
         self.setImage(self.image)
 
-        # Link view with profile plots
-        view = self.getView()
-        view.setXLink(self.main_window.x_plot_widget)
-        view.setYLink(self.main_window.y_plot_widget)
+        # Get viewing window
+        self.view = self.getView()
+        self.view.setLimits(
+            xMin=0,
+            xMax=(self.image.shape[0] + 50),
+            yMin=0,
+            yMax=(self.image.shape[1] + 50)
+        )
 
-        self.displayProfiles()
+        # Link view to profile plots
+        self.view.setXLink(self.main_window.x_plot_widget)
+        self.view.setYLink(self.main_window.y_plot_widget)
+        self.main_window.y_plot_widget.invertY(True)
 
+        # Dynamically update plots when viewing window changes
+        self.view.sigRangeChanged.connect(self.updatePlots)
 
-    def displayProfiles(self):
-        # Clear plots
-        self.main_window.x_plot_widget.clear()
-        self.main_window.y_plot_widget.clear()
+        # First plot
+        self.updatePlots()
 
-        # Mean of column
-        cols = self.image.mean(axis=1)
-        rows = self.image.mean(axis=0)
+    # --------------------------------------------------------------------------
+
+    def updatePlots(self):
+        # Coords visible in viewing window
+        self.view_range = self.view.viewRange()
+
+        # Limits of viewing window
+        x_min, x_max = max(int(self.view_range[0][0]), 0), int(self.view_range[0][1])
+        y_min, y_max = max(int(self.view_range[1][0]), 0), int(self.view_range[1][1])
+
+        # Embed block of image into array of zeros
+        # Able to take avg intensity of that part
+        view_image = np.zeros(self.image.shape, dtype=np.int)
+        im_part = self.image[x_min:x_max, y_min:y_max]
+        if 0 in im_part.shape:
+            return
+        view_image[x_min:x_min + im_part.shape[0], y_min:y_min + im_part.shape[1]] = im_part
+
+        # Mean of columns/rows
+        cols = view_image.mean(axis=1)
+        rows = view_image.mean(axis=0)
 
         # RGBA to grayscale conversion
-        # Contain avg intensity (0-255) of each row/column
+        # Calculate avg intensity (0-255) of each row/column
         col_avgs = 0.299 * cols[:,0] + 0.587 * cols[:,1] + 0.114 * cols[:,2]
         row_avgs = 0.299 * rows[:,0] + 0.587 * rows[:,1] + 0.114 * rows[:,2]
 
-        # Display profile plots
+        # Clear previous plots
+        self.main_window.x_plot_widget.clear()
+        self.main_window.y_plot_widget.clear()
+
+        # Display new plots
         self.main_window.x_plot_widget.plot(col_avgs)
         self.main_window.y_plot_widget.plot(row_avgs, range(len(row_avgs)))
 
