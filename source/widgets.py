409f8fcc91d2ace1f955bef755488bbdcc9e2a8a
henryjsmith12 <henryjsmith12@gmail.com>
2021-06-18 12:30:23 -0500

    Documentation for rest of widgets

--- source/widgets.py
+++ source/widgets.py
@@ -33,7 +33,7 @@ class OptionsWidget(pg.LayoutWidget):
 
     - Plotting options: The user can choose a variety of options to customize
         their image viewing experience:
-            - ROI toggle/color picker
+            - ROI toggle/** TODO: color picker
             - Mouse crosshair toggle
             - Mouse zoom/pan mode toggle
             - Background color toggle
@@ -124,8 +124,7 @@ class OptionsWidget(pg.LayoutWidget):
 
         # Create options widgets
         self.roi_boxes_chkbox = QtGui.QCheckBox("ROI")
-        self.crosshair_lbl = QtGui.QLabel("Crosshair:")
-        self.crosshair_mouse_chkbox = QtGui.QCheckBox("Mouse")
+        self.crosshair_mouse_chkbox = QtGui.QCheckBox("Crosshair")
         self.mouse_mode_lbl = QtGui.QLabel("Mouse Mode:")
         self.mouse_mode_group = QtGui.QButtonGroup()
         self.mouse_pan_rbtn = QtGui.QRadioButton("Pan")
@@ -182,8 +181,7 @@ class OptionsWidget(pg.LayoutWidget):
         self.remote_image_layout.addWidget(self.remote_z_spinbox, 5, 1)
         self.remote_image_layout.addWidget(self.remote_z_slider, 5, 2, 1, 3)
         self.options_layout.addWidget(self.roi_boxes_chkbox, 0, 0)
-        self.options_layout.addWidget(self.crosshair_lbl, 1, 0, 1, 1)
-        self.options_layout.addWidget(self.crosshair_mouse_chkbox, 1, 1)
+        self.options_layout.addWidget(self.crosshair_mouse_chkbox, 1, 0)
         self.options_layout.addWidget(self.mouse_mode_lbl, 2, 0)
         self.options_layout.addWidget(self.mouse_pan_rbtn, 2, 1)
         self.options_layout.addWidget(self.mouse_rect_rbtn, 2, 2)
@@ -524,6 +522,18 @@ class OptionsWidget(pg.LayoutWidget):
 
 class AnalysisWidget(pg.LayoutWidget):
 
+    """
+    Contains mouse, image, and ROI information.
+
+    - Mouse: Shows coordinates and pixel intesity for mouse position on image.
+
+    - Images: Shows image width/height, maximum pixel intensity, and colormap pctl.
+
+    - ROI: Spinboxes for box position and size that give the user a second method
+        of altering ROI's. Although visually housed in the analysis widget, the
+        ROI widgets are initialized in the ImageWidget __init__ function.
+    """
+
     def __init__ (self, parent):
         super(AnalysisWidget, self).__init__(parent)
         self.main_window = parent
@@ -561,6 +571,12 @@ class AnalysisWidget(pg.LayoutWidget):
     # --------------------------------------------------------------------------
 
     def setupComponents(self):
+
+        """
+        - Creates subwidgets
+        - Adds subwidgets to widget
+        """
+
         # Create mouse widgets
         self.mouse_x_lbl = QtGui.QLabel("x Pos:")
         self.mouse_x_txtbox = QtGui.QLineEdit()
@@ -593,7 +609,6 @@ class AnalysisWidget(pg.LayoutWidget):
         self.mouse_layout.addWidget(self.mouse_y_txtbox, 1, 1)
         self.mouse_layout.addWidget(self.mouse_intensity_lbl, 2, 0)
         self.mouse_layout.addWidget(self.mouse_intensity_txtbox, 2, 1)
-
         self.image_layout.addWidget(self.image_width_lbl, 0, 0)
         self.image_layout.addWidget(self.image_width_txtbox, 0, 1)
         self.image_layout.addWidget(self.image_height_lbl, 1, 0)
@@ -607,34 +622,39 @@ class AnalysisWidget(pg.LayoutWidget):
 
 class ImageWidget(pg.PlotWidget):
 
+    """
+    Contains viewing window for images.
+    """
+
     def __init__ (self, parent):
         super(ImageWidget, self).__init__(parent)
         self.main_window = parent
 
-        #self.hideAxis("left")
-        #self.hideAxis("bottom")
+        # x:y ratio set to 1
         self.setAspectLocked(True)
+        # Background initially set to black
         self.setBackground("default")
 
         self.view = self.getViewBox()
         self.image_item = pg.ImageItem()
         self.addItem(self.image_item)
 
-        # Set current cmap/cmap scaling
+        # Sets current cmap/cmap scaling
         self.cmap_scale = "Linear"
         self.cmap_pctl = 1.0
 
+        # Creates and adds ROIWidgets
+        # See ROIWidget class for more information
         self.roi1 = ROIWidget([200, 100], [40, 40], self.main_window.analysis_widget.roi1_layout)
         self.roi2 = ROIWidget([205, 105], [30, 30], self.main_window.analysis_widget.roi2_layout)
         self.roi3 = ROIWidget([210, 110], [20, 20], self.main_window.analysis_widget.roi3_layout)
         self.roi4 = ROIWidget([215, 115], [10, 10], self.main_window.analysis_widget.roi4_layout)
-
         self.addItem(self.roi1)
         self.addItem(self.roi2)
         self.addItem(self.roi3)
         self.addItem(self.roi4)
 
-        # Create mouse crosshair
+        # Creates mouse crosshair
         self.v_line = pg.InfiniteLine(angle=90, movable=False)
         self.h_line = pg.InfiniteLine(angle=0, movable=False)
         self.v_line.setVisible(False)
@@ -642,7 +662,7 @@ class ImageWidget(pg.PlotWidget):
         self.addItem(self.v_line, ignoreBounds=True)
         self.addItem(self.h_line, ignoreBounds=True)
 
-        # Create roi crosshair
+        # Creates roi crosshair
         self.roi_v_line = pg.InfiniteLine(angle=90, movable=False)
         self.roi_h_line = pg.InfiniteLine(angle=0, movable=False)
         self.roi_v_line.setVisible(False)
@@ -656,16 +676,23 @@ class ImageWidget(pg.PlotWidget):
 
     def displayImage(self, image):
 
+        """
+        Adds image to plot window with correct options.
+        """
+
         self.image = image
 
+        # Checks colormap scale
         if self.cmap_scale == "Log":
             norm = colors.LogNorm(vmax=np.amax(self.image)*self.cmap_pctl)
         else:
             norm = colors.Normalize(vmax=np.amax(self.image)*self.cmap_pctl)
 
-        # vmax=np.amax(self.image)
+        # Normalizes image
         norm_image = norm(self.image)
+        # Adds colormap to image
         color_image = plt.cm.jet(norm_image)
+        # Sets image
         self.image_item.setImage(color_image)
 
         # Update analysis textboxes
@@ -680,6 +707,11 @@ class ImageWidget(pg.PlotWidget):
     # --------------------------------------------------------------------------
 
     def updateMouseCrosshair(self, scene_point):
+
+        """
+        Changes crosshair information and updates lines.
+        """
+
         # View coordinates to plot coordinates
         view_point = self.view.mapSceneToView(scene_point)
 
@@ -693,6 +725,7 @@ class ImageWidget(pg.PlotWidget):
         self.main_window.analysis_widget.mouse_x_txtbox.setText(str(int(x)))
         self.main_window.analysis_widget.mouse_y_txtbox.setText(str(int(y)))
 
+        # Checks if mouse is within confines of image
         if self.image.shape[0] >= x >= 0 and self.image.shape[1] >= y >= 0:
             intensity = self.image[int(x), int(y)]
             self.main_window.analysis_widget.mouse_intensity_txtbox.setText(str(intensity))
@@ -701,19 +734,28 @@ class ImageWidget(pg.PlotWidget):
 
 class ROIWidget(pg.ROI):
 
+    """
+    Contains ROI for viewing window and layout for its respective groupbox in the
+    analysis widget.
+    """
+
     def __init__ (self, position, size, layout):
         super().__init__(position, size=size)
 
         self.pen = pg.mkPen("m", width=2)
         self.setPen(self.pen)
 
+        # For vertical, horizontal, and diagonal scaling
         self.addScaleHandle([0.5, 0], [0.5, 0.5])
         self.addScaleHandle([0, 0.5], [0.5, 0.5])
         self.addScaleHandle([0, 0], [0.5, 0.5])
+
+        # Intially hide the ROI
         self.hide()
 
         self.layout = layout
 
+        # Creates subwidgets for groupbox
         self.x_lbl = QtGui.QLabel("x Pos:")
         self.x_spinbox = QtGui.QSpinBox()
         self.x_spinbox.setMinimum(0)
@@ -731,6 +773,7 @@ class ROIWidget(pg.ROI):
         self.height_spinbox.setMinimum(0)
         self.height_spinbox.setMaximum(1000)
 
+        # Adds subwidgets to layout
         self.layout.addWidget(self.x_lbl, 0, 0)
         self.layout.addWidget(self.x_spinbox, 0, 1)
         self.layout.addWidget(self.y_lbl, 1, 0)
@@ -740,6 +783,7 @@ class ROIWidget(pg.ROI):
         self.layout.addWidget(self.height_lbl, 3, 0)
         self.layout.addWidget(self.height_spinbox, 3, 1)
 
+        # Connects subwidgets to signals
         self.sigRegionChanged.connect(self.updateAnalysis)
         self.visibleChanged.connect(self.updateAnalysis)
         self.width_spinbox.valueChanged.connect(self.updateSize)
@@ -749,11 +793,17 @@ class ROIWidget(pg.ROI):
 
         # Keeps track of whether textboxes or roi was updated last
         # Helps avoid infinite loop of updating
+        # Acts like a semaphore of sorts
         self.updating = ""
 
     # --------------------------------------------------------------------------
 
     def updateAnalysis(self):
+
+        """
+        Updates spinboxes in analysis widget.
+        """
+
         if self.updating != "roi":
             self.updating = "analysis"
             self.x_spinbox.setValue(self.pos()[0] + self.size()[0] / 2)
@@ -765,6 +815,11 @@ class ROIWidget(pg.ROI):
     # --------------------------------------------------------------------------
 
     def updateSize(self):
+
+        """
+        Updates ROI size in viewing window.
+        """
+
         if self.updating != "analysis":
             self.updating = "roi"
             width = self.width_spinbox.value()
@@ -776,6 +831,11 @@ class ROIWidget(pg.ROI):
     # --------------------------------------------------------------------------
 
     def updatePosition(self):
+
+        """
+        Updates ROI position in viewing window.
+        """
+
         if self.updating != "analysis":
             self.updating = "roi"
             # Bottom lefthand corner of roi
