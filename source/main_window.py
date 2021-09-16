995c75e832a5ce10dbf2e219346bf6250f84254a
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2021-09-16 12:36:28 -0500

    Comments

--- source/main_window.py
+++ source/main_window.py
@@ -13,6 +13,11 @@ from source.post_widget import *
 # ==============================================================================
 
 class MainWindow(QtGui.QMainWindow):
+    """
+    Creates the main window, which houses:
+    - A tab widget that houses widget tabs
+    - A menu bar that allows users to add new widget tabs
+    """
 
     def __init__ (self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -34,17 +39,23 @@ class MainWindow(QtGui.QMainWindow):
         self.new_menu_action = self.file_menu_item.addAction("New")
         self.help_menu_item = self.menu_bar.addMenu(" Help")
 
-        self.new_menu_action.triggered.connect(self.newWidgetTab)
-        # Remove tab
+        # Signals
+        self.new_menu_action.triggered.connect(self.newWidgetTab) # Opens dialog
         self.tab_widget.tabCloseRequested.connect(lambda index: \
-            self.tab_widget.removeTab(index))
+            self.tab_widget.removeTab(index)) # Remove tab
 
+        # Tabs open on initial startup (default)
         self.tab_widget.addTab(LivePlottingWidget(), "Live Plotting")
         self.tab_widget.addTab(PostPlottingWidget(), "Post Plotting")
 
     # --------------------------------------------------------------------------
 
     def newWidgetTab(self):
+        """
+        - Opens new widget dialog
+        - Creates instance of selected widget
+        - Houses widget in tab
+        """
 
         dialog = WidgetSelectionDialog()
         tab = None
@@ -60,6 +71,11 @@ class MainWindow(QtGui.QMainWindow):
 # ==============================================================================
 
 class WidgetSelectionDialog(QtGui.QDialog):
+    """
+    Dialog that allows user to:
+    - Select type of new widget
+    - Select name for new widget tab
+    """
 
     def __init__ (self):
         super().__init__()
@@ -82,8 +98,8 @@ class WidgetSelectionDialog(QtGui.QDialog):
         self.layout.addWidget(self.widget_name_txtbox, 1, 1)
         self.layout.addWidget(self.ok_btn, 2, 1)
 
+        # Signals
         self.ok_btn.clicked.connect(self.acceptDialog)
-        # Change widget name text to default
         self.widget_type_cbox.currentTextChanged.connect(lambda text: \
             self.widget_name_txtbox.setText(text))
 
@@ -92,6 +108,11 @@ class WidgetSelectionDialog(QtGui.QDialog):
     # --------------------------------------------------------------------------
 
     def acceptDialog(self):
+        """
+        - Creates new widget tab
+        - Triggered by clicking ok_btn
+        """
+        
         if self.widget_type_cbox.currentText() != "":
             self.widget_type = self.widget_type_cbox.currentText()
             self.widget_name = self.widget_name_txtbox.text()
